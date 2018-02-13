fc74538c7fc733d337c7f562cea1997654396fe0
NotMichaelChen <mzc12345@gmail.com>
2018-02-12 21:06:48 -0500

    Added Entities to program

---
+++ hw2/main.cpp
@@ -0,0 +1,71 @@
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+
+#include "entity.hpp"
+
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+SDL_Window *displayWindow;
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+    glewInit();
+
+    glViewport(0, 0, 1280, 720);
+
+    //Enable blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    //Declare matrices
+    Matrix projectionMatrix;
+    Matrix modelMatrix;
+    Matrix viewMatrix;
+
+    //Load shader program
+    ShaderProgram program;
+    program.Load("vertex.glsl", "fragment.glsl");
+    program.SetColor(1, 1, 1, 1);
+    glUseProgram(program.programID);
+
+    //Set orthographic projection
+    projectionMatrix.SetOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+
+    //Create entities
+    Entity foo(0, 0, 0, 0.5, 0.5);
+    Entity bar(1, 1, 0, 0.5, 0.5);
+
+    SDL_Event event;
+    bool done = false;
+    while (!done)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+            {
+                done = true;
+            }
+        }
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        foo.Draw(program);
+        bar.Draw(program);
+
+        SDL_GL_SwapWindow(displayWindow);
+    }
+
+    SDL_Quit();
+    return 0;
+}
