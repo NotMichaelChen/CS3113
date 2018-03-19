0b0b5ad592ca5b2a53d8f8758a1b883cc1827614
NotMichaelChen <mzc12345@gmail.com>
2018-03-19 14:56:16 -0400

    Created hw4 template

---
+++ hw4/main.cpp
@@ -0,0 +1,54 @@
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+
+#include "ShaderProgram.h"
+#include "Matrix.h"
+
+SDL_Window* displayWindow;
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("Platformer Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+    glewInit();
+
+    glViewport(0, 0, 1280, 720);
+
+    //Enable blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    //Load shader program
+    ShaderProgram program;
+    program.Load("vertex_textured.glsl", "fragment_textured.glsl");
+    program.SetColor(1, 1, 1, 1);
+    glUseProgram(program.programID);
+
+    //Declare matrices
+    Matrix projectionMatrix;
+    Matrix viewMatrix;
+
+    //Set matrices
+    projectionMatrix.SetOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+
+    SDL_Event event;
+    bool done = false;
+    while (!done) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+                done = true;
+            }
+        }
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(displayWindow);
+    }
+
+    SDL_Quit();
+    return 0;
+}
