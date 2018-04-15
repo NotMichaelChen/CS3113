6eb7d4d00f5eaa60437e5e9c6a89ba535f4b6651
NotMichaelChen <mzc12345@gmail.com>
2018-04-14 23:06:20 -0400

    Filled out gamestate class methods

--- final/states/gamestate.cpp
+++ final/states/gamestate.cpp
@@ -1,4 +1,39 @@
 #include "gamestate.hpp"
 
+#include "util.hpp"
+#include "SheetSprite.hpp"
+
 GameState::GameState(ShaderProgram* prg) : program(prg) {
+    keys = SDL_GetKeyboardState(NULL);
+
+    ship_spritesheet = LoadTexture("./assets/sheet.png", GL_NEAREST);
+    //playerShip3_blue.png
+    SheetSprite playersprite(ship_spritesheet, 325, 739, 98, 75, 0.35, 1024, 1024);
+    player = std::make_shared<PlayerEntity>(playersprite, keys);
+}
+
+Global::ProgramStates GameState::processEvents() {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event))
+    {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+        {
+            return Global::ProgramStates::Quit;
+        }
+        else if(event.type == SDL_KEYDOWN) {
+            if(event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+                return Global::ProgramStates::Menu;
+            }
+        }
+    }
+    return Global::ProgramStates::Game;
+}
+
+void GameState::update(float elapsed) {
+    player->Update(elapsed);
+}
+
+void GameState::render() {
+    player->Draw(program);
 }
--- final/states/gamestate.hpp
+++ final/states/gamestate.hpp
@@ -1,14 +1,33 @@
 #ifndef GAMESTATE_HPP
 #define GAMESTATE_HPP
 
+#include <GL/glew.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+
+#include <memory>
+
+#include "global.hpp"
 #include "ShaderProgram.h"
+#include "entities/player.hpp"
 
 class GameState {
 public:
     GameState(ShaderProgram* prg);
+    Global::ProgramStates processEvents();
+    void update(float elapsed);
+    void render();
 
 private:
     ShaderProgram* program;
+    const Uint8* keys;
+
+    GLuint ship_spritesheet;
+
+    //Making player a smart pointer allows me to defer construction until the constructor body. This lets me load the sprite
+    //before constructing the player
+    std::shared_ptr<PlayerEntity> player;
 };
 
 #endif
