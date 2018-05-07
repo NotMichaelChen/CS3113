84508295d35c9737e1718344ffcd9b33738578f8
NotMichaelChen <mzc12345@gmail.com>
2018-05-07 19:55:43 -0400

    Added bursts to advanced phase 2

--- final/phases/phase_advanced.cpp
+++ final/phases/phase_advanced.cpp
@@ -56,7 +56,7 @@ void advancedPhaseTwo(BossEntity* boss, float elapsed) {
     const int FIRETICKS = 100;
     const int WAITTICKS = 0;
     const int MOVETICKS = 60;
-    const int FIREDELAY = 20;
+    const int FIREDELAY = 10;
 
     //for convenience
     PhaseData* data = &(boss->data);
@@ -71,22 +71,33 @@ void advancedPhaseTwo(BossEntity* boss, float elapsed) {
         data->statenum = (data->statenum+1) % 3;
     }
 
-    if(data->statenum == 0 && data->localticks % FIREDELAY == 0) {
-        SheetSprite bulletsprite(Global::bullet_spritesheet, 112, 49, 16, 16, 0.1, 1024, 1024);
-        std::vector<Bullet> newbullets;
+    if(data->statenum == 0) {
+        //Bursts
+        if(data->localticks == 0) {
+            Vec genpos;
+            genpos.x = randFloat(-2, 2);
+            genpos.y = randFloat(-0.5, 1);
 
-        int pattern = randInt(0, 1);
-        if(pattern == 0 && data->localticks) {
-            newbullets = generateCircle(bulletsprite, boss->position, 0.5, 25, 0);
+            SheetSprite generatorsprite(Global::bullet_spritesheet, 288, 0, 32, 32, 0.1, 1024, 1024);
+            SheetSprite bulletsprite(Global::bullet_spritesheet, 112, 49, 16, 16, 0.1, 1024, 1024);
+            GeneratorEntity genent(generatorsprite, bulletsprite, genpos, 0, 30, 60, 5, boss->bullets);
+            
+            boss->generators->push_back(genent);
         }
-        else if(pattern == 1 && data->localticks) {
-            newbullets = generateLineSpread(bulletsprite, boss->position, {1.5, -0.6}, 15);
-        }
-        // else if(pattern == 2 && data->localticks % FIREDELAY == 0) {
-        //     generateSingle(bulletsprite, boss->position, Global::PI, 0.8);
-        // }
+        if(data->localticks % FIREDELAY == 0) {
+            SheetSprite bulletsprite(Global::bullet_spritesheet, 112, 49, 16, 16, 0.1, 1024, 1024);
+            std::vector<Bullet> newbullets;
 
-        boss->bullets->insert(boss->bullets->end(), newbullets.begin(), newbullets.end());
+            int pattern = randInt(0, 1);
+            if(pattern == 0 && data->localticks) {
+                newbullets = generateCircle(bulletsprite, boss->position, 0.5, randInt(20, 25), 0);
+            }
+            else if(pattern == 1 && data->localticks) {
+                newbullets = generateLineSpread(bulletsprite, boss->position, {1.5, -0.5}, randInt(5, 15));
+            }
+
+            boss->bullets->insert(boss->bullets->end(), newbullets.begin(), newbullets.end());
+        }
     }
     //Use is_moving to indicate if we've computed the next location yet
     else if(data->statenum == 1 && !data->is_moving) {
