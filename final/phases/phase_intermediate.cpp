cf9d8b378fed7b8ce9e22d60e2a39cdb6f5c17a6
NotMichaelChen <mzc12345@gmail.com>
2018-05-07 21:12:03 -0400

    Moved phases around

--- final/phases/phase_intermediate.cpp
+++ final/phases/phase_intermediate.cpp
@@ -91,13 +91,34 @@ void intermediatePhaseTwo(BossEntity* boss, float elapsed) {
 }
 
 void intermediatePhaseThree(BossEntity* boss, float elapsed) {
+    //States: 0=moveintoplace, 1=waiting, 2=sweepleft, 3=waiting, 4=sweepright,
+    const int FIRETICKS = 100;
+    const int WAITTICKS = 30;
     const int MOVETICKS = 60;
+    const int FIREDELAY = 10;
 
     //for convenience
     PhaseData* data = &(boss->data);
 
-    //Part one: move into position
-    if(data->localticks < MOVETICKS) {
+    //Update state
+    if(data->statenum == 0 && data->localticks >= MOVETICKS) {
+        data->localticks = 0;
+        data->is_moving = false;
+        data->statenum++;
+    }
+    if((data->statenum == 1 && data->localticks >= WAITTICKS) ||
+        (data->statenum == 2 && data->localticks >= FIRETICKS) ||
+        (data->statenum == 3 && data->localticks >= WAITTICKS) ||
+        (data->statenum == 4 && data->localticks >= FIRETICKS))
+    {
+        data->localticks = 0;
+        data->is_moving = false;
+        data->statenum = (data->statenum+1) % 5;
+        if(data->statenum == 0)
+            data->statenum++;
+    }
+
+    if(data->statenum == 0) {
         if(!data->is_moving) {
             data->origin = boss->position;
             data->destination = Vec(0,1.5);
@@ -109,29 +130,20 @@ void intermediatePhaseThree(BossEntity* boss, float elapsed) {
             boss->position = easeInOut(data->origin, data->destination, adjustedtime);
         }
     }
-    //Part two: Generate lines & random bursts
-    else {
-        //Lines
-        if(data->localticks % 25 == 0) {
-            const int ARMS = 8;
-
+    else if(data->statenum == 2) {
+        if(data->localticks % FIREDELAY == 0) {
             SheetSprite bulletsprite(Global::bullet_spritesheet, 112, 49, 16, 16, 0.1, 1024, 1024);
-
-            for(int i = 1; i < ARMS; i++) {
-                boss->bullets->push_back(generateSingle(bulletsprite, boss->position, Global::PI + (Global::PI*i)/ARMS, 0.8));
-            }
+            float angle = Global::PI + data->localticks/(10*Global::PI);
+            std::vector<Bullet> newbullets = generateStaticCircle(bulletsprite, boss->position, 0.6, angle, 0.7, 15);
+            boss->bullets->insert(boss->bullets->end(), newbullets.begin(), newbullets.end());
         }
-        //Bursts
-        if(data->localticks % 80 == 0) {
-            Vec genpos;
-            genpos.x = randFloat(-2, 2);
-            genpos.y = randFloat(-0.5, 1);
-
-            SheetSprite generatorsprite(Global::bullet_spritesheet, 288, 0, 32, 32, 0.1, 1024, 1024);
+    }
+    else if(data->statenum == 4) {
+        if(data->localticks % FIREDELAY == 0) {
             SheetSprite bulletsprite(Global::bullet_spritesheet, 112, 49, 16, 16, 0.1, 1024, 1024);
-            GeneratorEntity genent(generatorsprite, bulletsprite, genpos, 0, 30, 60, 5, boss->bullets);
-            
-            boss->generators->push_back(genent);
+            float angle = -data->localticks/(10*Global::PI);
+            std::vector<Bullet> newbullets = generateStaticCircle(bulletsprite, boss->position, 0.6, angle, 0.7, 15);
+            boss->bullets->insert(boss->bullets->end(), newbullets.begin(), newbullets.end());
         }
     }
 
